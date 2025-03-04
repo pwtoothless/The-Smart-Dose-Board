4e5dc045068a0ba05d7ffc30ce28b740ab397ad1
pwtoothless <70607768+pwtoothless@users.noreply.github.com>
2025-03-04 07:49:54 -0800

    Now should write to file and console

--- Scale_Code.py
+++ Scale_Code.py
@@ -1,21 +1,25 @@
 from hx711 import HX711
+import RPi.GPIO as GPIO
+hx711 = HX711(
+    dout_pin=5,
+    pd_sck_pin=6,
+    channel='A',
+    gain=64
+)
 
-try:
-    hx711 = HX711(
-        dout_pin=5,
-        pd_sck_pin=6,
-        channel='A',
-        gain=64
-    )
-
-    hx711.reset()   # Before we start, reset the HX711 (not obligate)
-    measures = hx711.get_raw_data(num_measures=3)
-finally:
-    GPIO.cleanup()  # always do a GPIO cleanup in your scripts!
+hx711.reset()   # Before we start, reset the HX711 (not obligate)
+measures = hx711.get_raw_data(num_measures=3)
+GPIO.cleanup()  # always do a GPIO cleanup in your scripts!
 
 text = '\n'
+print("Weight Measurement:")
+for measure in measures:
+    text += str(measure) + '\n'
+    print(measure)
+    print(text)
 
 with open('./weight.txt', 'w') as f:
+    f.write(measure)
     f.write(text)
     f.close()
 
